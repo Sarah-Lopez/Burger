80282da9dcd84983be3c4b5375979f5f6425f9c0
Sarah Lopez <Sarahanne.pez@gmail>
2019-08-25 23:47:31 -0700

    Initial files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avacado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
